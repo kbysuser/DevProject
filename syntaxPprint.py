dba5dc9f55e82a86513d53aa295f5b665c35c2d8
kbysuser <ryo84log@gmail.com>
2024-05-27 21:58:17 +0900

    commited

--- syntaxPprint.py
+++ syntaxPprint.py
@@ -9,7 +9,7 @@ file_path=eg.popup_get_file(
 )
 
 
-with open(file_path, 'r') as file:
+with open(file_path, mode='r',encoding="UTF-8") as file:
     # Read the contents of the file
     file_contents = file.read()
     # Pretty print the contents
@@ -19,16 +19,18 @@ with open(file_path, 'r') as file:
 
 layout = [
     [eg.Text("Copy pretty-printed json.")],
-    [eg.InputText(pretty_json ,key="-contents-")],
+    [eg.Textarea(pretty_json ,key="-contents-", 
+                 size = (50, 10),expand_x=True,expand_y=True)],
     [eg.Button("OK"),eg.Button("COPY")]
 ]
 window = eg.Window("Instagram", layout=layout)
+window.size=("100","200")
 
 while window.is_alive():
     # イベントと値を取得
     event, values = window.read()
     # OKボタンを押した時
-    if event == "OK":
+    if event in ["OK", eg.WINDOW_CLOSED]:
         break
     if event == "COPY":
         # pyperclip.copy(values["-contents-"])
