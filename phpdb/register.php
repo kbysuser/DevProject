548ef1ec55cd9213ebbe76471e763bd6ea0d6767
kbysuser <ryo84log@gmail.com>
2025-03-23 14:35:28 +0900

    DB連携のプロトタイプ(仕掛品)

---
+++ phpdb/register.php
@@ -0,0 +1,33 @@
+<?php 
+//データベース接続情報
+$host='localhost';
+$dbname='xxx';
+$user='root';
+$password='password';
+
+try{
+    //PDOでデータベースに接続
+    $pdo=new PDO("mysql:host={$host};dbname={$dbname};charset=utf-8",$user,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //フォームデータの取得
+    $username=$_POST['username'];
+    $questionId=$_POST['questionId'];
+    $answer=$_POST['answer'];
+    $comment=$_POST['comment'];
+    //SQLの準備と実行
+    $sql="insert into answers(username,questionId,answer,comment) "
+        ."values(:username,:questionId,:answer,:comment)";
+    $stmt=$pdo->prepare($sql);
+    $stmt->bindParam(':username',$username);
+    $stmt->bindParam(':questionId',$questionId);
+    $stmt->bindParam(':answer',$answer);
+    $stmt->bindParam(':comment',$comment);
+    //SQLの実行
+    $stmt->execute();
+    echo "登録が完了しました";
+}catch(PDOException $e){
+    echo "<h1>エラー：".$e->getMessage()."</h1>";
+}
+
+
+?>
