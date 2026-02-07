648909671eb9ea9674474e9f10d57e091b165286
kbysuser <ryo84log@gmail.com>
2026-02-07 16:24:52 +0900

    log.txt

--- kangenkyouyou/practice/submit.php
+++ kangenkyouyou/practice/submit.php
@@ -8,12 +8,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $name = $_POST["name"];
         $password = $_POST["password"];
-        $msg= "Your name is \"{$name}\" "
+        $msg = "Your name is \"{$name}\" "
             . "and your password is \"{$password}\"";
+        date_default_timezone_set("Asia/Tokyo");
+        $date = date("Y-m-d H:i:s");
+        $log = "date=$date&name=$name&password=$password";
+        file_put_contents("./log.txt", $log . "\n", FILE_APPEND);
     }
 }
 ?>
-<div style="display: flex;justify-content: center;"> 
+<div style="display: flex;justify-content: center;">
     <h1>My Page</h1>
 </div>
 <fieldset style="background-color: lightcyan;">
