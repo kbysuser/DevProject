592c89dee0925c5ad8e4bdb3633d89a3d1fddc8b
kbysuser <ryo84log@gmail.com>
2026-04-29 12:41:37 +0900

    decrypted.js

--- kangenkyouyou/AdBlockTest/content.js
+++ kangenkyouyou/AdBlockTest/content.js
@@ -1,8 +1,12 @@
 
 // overflow: hiddenを無効化
-const sheet = new CSSStyleSheet();
-sheet.replaceSync('html, body { overflow: auto !important; height: auto !important; }');
-document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+try {
+    sheet = new CSSStyleSheet();
+    sheet.replaceSync('html, body { overflow: auto !important; height: auto !important; }');
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+} catch (e) {
+
+}
 
 
 // sheet.replaceSync('html, body { overflow: auto !important; height: auto !important; padding: 0 !important; }');
@@ -10,11 +14,19 @@ document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
 
 
 // スクリプト、iframe、広告タグを削除
-document.querySelectorAll('script, iframe, ins, ads,[class*=advertise],div.fc-message-root').forEach(el => el.remove());
+document.querySelectorAll('script, iframe, ins, ads,[class*=advertise],div.fc-message-root').forEach(el => el?.remove());
+
+//weblio用
+document.querySelectorAll('div[id*=yads]').forEach(el => el?.remove());
+document.querySelectorAll('div[id*=yads]').forEach(el => el?.remove());
+document.querySelectorAll("[id*=gn_interstitial_outer_area],[id*=google_ads_iframe],video").forEach(el => el?.remove());
+document.getElementById("footer-overlay")?.remove();
+document.getElementById("footFixAdBar")?.remove();
+document.getElementById("videoWrapperDiv")?.remove();
 
 // タイマー停止 
-let id = setTimeout(() => {}, 0);
-while (id--) clearTimeout(id);
+timerId = setTimeout(() => { }, 0);
+while (timerId--) clearTimeout(timerId);
 
 // 要素を再描画、イベントリスナーを全削除
 // (※注意: サイトによってはレイアウトが崩れる可能性
